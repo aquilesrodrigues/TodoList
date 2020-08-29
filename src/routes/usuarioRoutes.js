00260b2d6d8d28140ea55d800de47af87d7b4c1e
Aquiles Rodrigues <cyberfazer@gmail.com>
2020-08-29 01:46:42 -0300

    usar módulo exports para exportar rota do usuário

---
+++ src/routes/usuarioRoutes.js
@@ -0,0 +1,6 @@
+module.exports = function(app){
+    const usuarios = require("../controllers/usuariosController.js")
+    app.route("/usuarios")
+    .get(usuarios.listAll)
+    .post(usuarios.createone)
+}
